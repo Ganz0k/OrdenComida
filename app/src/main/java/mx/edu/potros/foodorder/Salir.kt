64f1d64dae36a1c07a10d3e6aeaff2b618679654
ximenaic <xmnislas@gmail.com>
2023-03-31 02:02:52 -0700

    pantalla lista orden

--- app/src/main/java/mx/edu/potros/foodorder/Salir.kt
+++ app/src/main/java/mx/edu/potros/foodorder/Salir.kt
@@ -1,11 +1,26 @@
 package mx.edu.potros.foodorder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Salir : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_salir)
+
+        val buttonSalir: Button = findViewById(R.id.btn_salir)
+        val buttonCancelar: Button = findViewById(R.id.btn_cancelar)
+
+        buttonSalir.setOnClickListener{
+            var intent: Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonCancelar.setOnClickListener{
+            var intent: Intent = Intent(this, MenuPrincipal::class.java)
+            startActivity(intent)
+        }
     }
 }
