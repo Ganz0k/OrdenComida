34045a4ebe92e989c51be068ab0cdb913d6c18c7
Ganz0k <luisgonzalocervantes@gmail.com>
2023-03-31 15:10:42 -0700

    some more flows

--- app/src/main/java/mx/edu/potros/foodorder/FinCuenta.kt
+++ app/src/main/java/mx/edu/potros/foodorder/FinCuenta.kt
@@ -1,11 +1,26 @@
 package mx.edu.potros.foodorder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class FinCuenta : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fin_cuenta)
+
+        val btnAgregarCuenta: Button = findViewById(R.id.btn_agregar_cuenta)
+        val btnIrPedido: Button = findViewById(R.id.btn_ir_pedido)
+
+        btnAgregarCuenta.setOnClickListener {
+            var intent = Intent(this, MenuOrdenar::class.java)
+            startActivity(intent)
+        }
+
+        btnIrPedido.setOnClickListener {
+            var intent = Intent(this, Cuenta::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/mx/edu/potros/foodorder/SeguirAgregando.kt
+++ app/src/main/java/mx/edu/potros/foodorder/SeguirAgregando.kt
@@ -1,11 +1,26 @@
 package mx.edu.potros.foodorder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SeguirAgregando : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seguir_agregando)
+
+        val btnSeguir: Button = findViewById(R.id.btn_seguir)
+        val btnFinalizar: Button = findViewById(R.id.btn_finalizar)
+
+        btnSeguir.setOnClickListener {
+            var intent = Intent(this, Menu::class.java)
+            startActivity(intent)
+        }
+
+        btnFinalizar.setOnClickListener {
+            var intent = Intent(this, FinCuenta::class.java)
+            startActivity(intent)
+        }
     }
 }
