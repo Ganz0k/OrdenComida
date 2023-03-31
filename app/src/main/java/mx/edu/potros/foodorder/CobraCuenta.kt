e95a615cbce0820fd68cfca7207c699ea7ebb429
Ganz0k <luisgonzalocervantes@gmail.com>
2023-03-31 15:37:55 -0700

    final flow

--- app/src/main/java/mx/edu/potros/foodorder/CobraCuenta.kt
+++ app/src/main/java/mx/edu/potros/foodorder/CobraCuenta.kt
@@ -1,11 +1,27 @@
 package mx.edu.potros.foodorder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CobraCuenta : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cobra_cuenta)
+
+        val btnAceptar: Button = findViewById(R.id.btn_aceptar)
+        val btnCancelar: Button = findViewById(R.id.btn_cancelar)
+
+        btnAceptar.setOnClickListener {
+            var intent = Intent(this, Confirmed::class.java)
+            intent.putExtra("tipo", "cobrar")
+            startActivity(intent)
+        }
+
+        btnCancelar.setOnClickListener {
+            var intent = Intent(this, MenuPrincipalLista::class.java)
+            startActivity(intent)
+        }
     }
 }
