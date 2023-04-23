7c0adac760dd9127c4cbad0b460f5788f2f0f930
Ganz0k <luisgonzalocervantes@gmail.com>
2023-04-22 19:26:04 -0700

    Log in v1.0
    Log in functionality fully implemented and somewhat tested. It warns the user if they troy to log in if they leave the fields blank, tells the user his username/password might be incorrect if it doesn't match with any user in the database and displays a log in successful message to the user if it does find the user in the database to finally take them to the "Bienvenido" screen

--- app/src/main/java/mx/edu/potros/foodorder/MainActivity.kt
+++ app/src/main/java/mx/edu/potros/foodorder/MainActivity.kt
@@ -4,8 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
+
+    private val userRef = FirebaseDatabase.getInstance().getReference("Users")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,13 +29,50 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnLogin.setOnClickListener {
-            var intent = Intent(this, Bienvenido::class.java)
-            startActivity(intent)
+            logIn()
         }
 
         btnRegistro.setOnClickListener {
             var intent = Intent(this, Registro::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun logIn() {
+        var etUsuario: EditText = findViewById(R.id.et_usuario)
+        var etPassword: EditText = findViewById(R.id.et_password)
+
+        if (etUsuario.text.isBlank() || etPassword.text.isBlank()) {
+            Toast.makeText(this, "Llene los campos antes de iniciar sesión", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        var usuario: String = etUsuario.text.toString().trim()
+        var password: String = etPassword.text.toString().trim()
+
+        userRef.orderByChild("correo").equalTo(usuario).addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    var usuarioExistente: User? = null
+
+                    for (s in snapshot.children) {
+                        usuarioExistente = s.getValue(User::class.java)
+
+                        if (usuarioExistente != null && usuarioExistente.password == password) {
+                            Toast.makeText(this@MainActivity, "Inicio de sesión exitoso", Toast.LENGTH_LONG).show()
+                            var intent = Intent(this@MainActivity, Bienvenido::class.java)
+                            startActivity(intent)
+                            return
+                        }
+                    }
+
+                    Toast.makeText(this@MainActivity, "Usuario y/o contraseña incorrectos", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this@MainActivity, "Usuario y/o contraseña incorrectos", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
     }
 }
